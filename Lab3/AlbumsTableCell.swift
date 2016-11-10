31c344d96876587bee84ffd9b74cc4a75b31e072
Michal <michal@Michals-iMac.local>
2016-11-10 21:15:10 +0100

    init

---
+++ Lab3/AlbumsTableCell.swift
@@ -0,0 +1,27 @@
+//
+//  AlbumsTableCell.swift
+//  Lab3
+//
+//  Created by Michal on 11/10/16.
+//  Copyright © 2016 Michal. All rights reserved.
+//
+
+import UIKit
+
+class AlbumsTableCell: UITableViewCell {
+
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var musicInfo: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        artistLabel.text = "Dupa";
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Lab3/ArtistsView.swift
@@ -0,0 +1,25 @@
+//
+//  ArtistsView.swift
+//  Lab3
+//
+//  Created by Michal on 11/10/16.
+//  Copyright © 2016 Michal. All rights reserved.
+//
+
+import UIKit
+
+class ArtistsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
