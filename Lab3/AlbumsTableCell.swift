500442c7273dec0c8f6704b0bd021fb089597bca
Michal <michal@Michals-iMac.local>
2016-11-10 21:29:40 +0100

    init

--- Lab3/AlbumsTableCell.swift
+++ Lab3/AlbumsTableCell.swift
@@ -14,6 +14,7 @@ class AlbumsTableCell: UITableViewCell {
     @IBOutlet weak var musicInfo: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
+        
         artistLabel.text = "Dupa";
         // Initialization code
     }
